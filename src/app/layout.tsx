9bf0ee7ec8895a6cdd3b62a6d48486ea6b9ba25f
JwanCortez93 <jwancortez@gmail.com>
2024-10-11 05:02:56 -0300

    Clerk integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "QuickSlot",
   description: "The fastest way to secure your time.",
-  icons: { icon: "" },
 };
 
 export default function RootLayout({
@@ -17,15 +17,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={cn(
-          "min-h-screen, bg-background font-sans antialiased",
-          inter.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={cn(
+            "min-h-screen, bg-background font-sans antialiased",
+            inter.variable
+          )}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
