0addae0aebd415704fc4b3d2d6a47e84c92a31bf
JwanCortez93 <jwancortez@gmail.com>
2024-10-11 06:55:42 -0300

    events route init

--- src/app/(private)/events/page.tsx
+++ src/app/(private)/events/page.tsx
@@ -1,10 +1,46 @@
-import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { db } from "@/drizzle/db";
+import { auth } from "@clerk/nextjs/server";
+import { CalendarPlus, CalendarX } from "lucide-react";
+import Link from "next/link";
 
-const EventsPage = () => {
+const EventsPage = async () => {
+  const { userId, redirectToSignIn } = auth();
+
+  if (userId === null) return redirectToSignIn();
+
+  const events = await db.query.EventTable.findMany({
+    where: ({ clerkUserId }, { eq }) => eq(clerkUserId, userId),
+    orderBy: ({ createdAt }, { desc }) => desc(createdAt),
+  });
+
   return (
     <>
-      <h1>Events</h1>
-      <UserButton />
+      <div className="flex gap-4 items-baseline">
+        <h1 className="text-3xl lg:text-4xl xl:text-5xl font-semibold mb-6">
+          Events
+        </h1>
+        <Button asChild>
+          <Link href="/events/new">
+            <CalendarPlus className="mr-4 size-6" />
+            New Event
+          </Link>
+        </Button>
+      </div>
+      {events.length > 0 ? (
+        <></>
+      ) : (
+        <div className="flex flex-col items-center gap-4">
+          <CalendarX className="size-16 mx-auto" />
+          You have no events yet.
+          <Button size="lg" className="text-lg" asChild>
+            <Link href="/events/new">
+              <CalendarPlus className="mr-4 size-6" />
+              New Event
+            </Link>
+          </Button>
+        </div>
+      )}
     </>
   );
 };
